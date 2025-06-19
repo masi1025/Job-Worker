1ef9ca12220f3856c30c36bec56f9541135f78aa
Sidra Mashlah <Sidra.mashlah@gmail.com>
2025-06-19 21:00:06 +0200

    anpassungen

--- index.ts
+++ index.ts
@@ -12,77 +12,28 @@ const camunda = new Camunda8({
 const zeebe = camunda.getZeebeGrpcApiClient({
 });
  zeebe.createWorker({
-  taskType: 'RisikoBewerten', // Task-Type wie im BPMN-Modell
+  taskType: 'RisikoBewerten',
   taskHandler: async (job) => {
     console.log('Worker "RisikoBewerten" gestartet');
-    const axios = require('axios');
+     let cancelled = false;
+    
 
+    const kundenDaten = job.variables;
 
-const id = job.variables.id;
+    // Anfrage an JSON-Server mit allen Daten
+    const response = await axios.post('http://localhost:3000/berechneRisiko', kundenDaten);
 
-const response = await axios.get(`http://localhost:3000/Kunden/${id}`);
-const risikoDaten = response.data;
-     
-     
-   const insolvenz = Boolean(risikoDaten.insolvenz);
-   const schufaScore = Number(risikoDaten.schufaScore);
-   const name=  String(risikoDaten.name);
-   const einkommen= Number(risikoDaten.einkommen);
-   const kreditsumme= Number(risikoDaten.kreditsumme);
-   const vermoegen= Number(risikoDaten.vermoegen);
-   const verbindlichkeiten = Number(risikoDaten.verbindlichkeiten);
+    const { id, risikokategorie } = response.data;
 
-
-    // --- 1. Risikoanalyse 
-    // Je mehr Vermögen, desto besser. Je mehr Schulden, desto schlechter.
-    // Je höher das Einkommen, desto niedriger das Risiko.
-
-    const nettovermoegen = Number(vermoegen) - Number(verbindlichkeiten);
-    const einkommensquote = Number(einkommen) / Number(kreditsumme); // Je höher desto besser
-    const risikoWert = (Number(kreditsumme) / (nettovermoegen + 1)) + (1 / (einkommensquote + 1));
-
-    let risikokategorie: 'Niedrig' | 'Mittel' | 'Hoch';
-
-     if (risikoWert < 0.5 && !insolvenz && schufaScore > 80) {
-      risikokategorie = 'Niedrig';
-     } else if (risikoWert < 1.2 && schufaScore > 60) {
-      risikokategorie = 'Mittel';
-    } else {
-      risikokategorie = 'Hoch';
-    }
-
-    // --- 2. Zinssatzberechnung (automatisiert) ---
-    let zinssatz: number;
-
-    switch (risikokategorie) {
-      case 'Niedrig':
-        zinssatz = 2.5;
-        break;
-      case 'Mittel':
-        zinssatz = 4.0;
-        break;
-      case 'Hoch':
-        zinssatz = 6.5;
-        break;
-    }
-
-    // Ausgabe und Rückgabe der neuen Variablen
-    const neueVariablen = {
-      id ,
+    console.log("Risikoanalyse abgeschlossen durch JSON Server:", {
       risikokategorie,
-      zinssatz
-    };
-
-    console.log("Risikoanalyse abgeschlossen:", {
-      risikokategorie,
-      //risikoWert: risikoWert.toFixed(2),
-      zinssatz: zinssatz.toFixed(2)
+      //zinssatz
     });
 
-    return job.complete(neueVariablen);
-  },
+    // Rückgabe an Camunda
+    return job.complete({
+      id,
+      risikokategorie
+    });
+  }
 });
-
-
-
-
